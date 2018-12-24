1503cb07b4f10343d8d285ef43cc1262706f7ff1
karabala <tigranharutyunyan59@gmail.com>
2018-12-24 02:40:39 -0800

    added edit

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
+
 const home = require('./routes/home')
 
-const app = express();
+const app = express()
+
+app.use(express.static('public'));
 
 app.set('view engine', 'pug')
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(home)
 
--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,39 @@
 const { Router } = require('express')
-
+const uuid = require('uuid/v4')
 const route = Router()
+const toDos = [];
+let editToDo
 
-route.get('/', (res, req) => {
-    res.render('toDosList')
+route.get('/', (req, res) => {
+    res.render('toDosList', { toDos })
+})
+
+route.post('/', (req, res) => {
+    toDos.push({ id: uuid(), task: req.body.task })
+    res.render('toDosList', { toDos })
+})
+
+route.get('/delete/:id', (req, res) => {
+    const { id } = req.params;
+    const index = toDos.findIndex(todo => todo.id === id)
+    toDos.splice(index, 1)
+    res.redirect('/')
+})
+
+route.get('/edit/:id', (req, res) => {
+    const { id } = req.params;
+    const index = toDos.findIndex(todo => todo.id === id)
+    editToDo = toDos[index]
+    res.redirect('/edit')
+})
+
+route.get('/edit', (req, res) => {
+    res.render('editToDo')
+})
+
+route.post('/edit', (req, res) => {
+    editToDo.task = req.body.task
+    res.redirect('/')
 })
 
 module.exports = route
